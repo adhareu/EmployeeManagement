134a1aae482ac8c17e8a756884cbfaecbc00d774
asif iqbal <iqbal.bd@asa-international.com>
2025-01-01 12:40:58 +0600

    addewd migrations

---
+++ EmployeeManagement_sln/EmployeeManagement.API/Migrations/20241231102413__initial.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class _initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "dbo");
+
+            migrationBuilder.CreateTable(
+                name: "Department",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModifiedBy = table.Column<long>(type: "bigint", nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Department", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Designation",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModifiedBy = table.Column<long>(type: "bigint", nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Designation", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    EmployeeCode = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    JoiningDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    DepartmentId = table.Column<int>(type: "int", nullable: false),
+                    DesignationId = table.Column<int>(type: "int", nullable: false),
+                    CreatedBy = table.Column<long>(type: "bigint", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    LastModifiedBy = table.Column<long>(type: "bigint", nullable: true),
+                    LastModifiedDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employee_Department_DepartmentId",
+                        column: x => x.DepartmentId,
+                        principalSchema: "dbo",
+                        principalTable: "Department",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_Employee_Designation_DesignationId",
+                        column: x => x.DesignationId,
+                        principalSchema: "dbo",
+                        principalTable: "Designation",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DepartmentId",
+                schema: "dbo",
+                table: "Employee",
+                column: "DepartmentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employee_DesignationId",
+                schema: "dbo",
+                table: "Employee",
+                column: "DesignationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Employee",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Department",
+                schema: "dbo");
+
+            migrationBuilder.DropTable(
+                name: "Designation",
+                schema: "dbo");
+        }
+    }
+}
---
+++ EmployeeManagement_sln/EmployeeManagement.API/Migrations/20250101033807__Updatealltable.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmployeeManagement.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Updatealltable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Employee");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Designation");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Designation");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Designation");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Designation");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Department");
+
+            migrationBuilder.DropColumn(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Department");
+
+            migrationBuilder.CreateTable(
+                name: "User",
+                schema: "dbo",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Login = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User",
+                schema: "dbo");
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Employee",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Employee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Employee",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Employee",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Designation",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Designation",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Designation",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Designation",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<long>(
+                name: "CreatedBy",
+                schema: "dbo",
+                table: "Department",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedDate",
+                schema: "dbo",
+                table: "Department",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<long>(
+                name: "LastModifiedBy",
+                schema: "dbo",
+                table: "Department",
+                type: "bigint",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastModifiedDate",
+                schema: "dbo",
+                table: "Department",
+                type: "datetime2",
+                nullable: true);
+        }
+    }
+}
